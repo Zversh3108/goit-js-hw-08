fd0eb144e86ffb610ac10c51834f46523af98a63
Zversh3108 <zvershzt3108@gmail.com>
2023-03-12 10:06:16 +0100

    8 za

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    galleryItem =>
+      `<a class="gallery__item" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+    </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
---
+++ src/js/03-feedback.js
@@ -0,0 +1,33 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+  formLogin: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('[type = email]'),
+  inputMessage: document.querySelector('[name = message]'),
+  submitButton: document.querySelector('[type = submit]'),
+};
+
+let userData = {};
+refs.formLogin.addEventListener(
+  'input',
+  throttle(event => {
+    
+  
+
+    userData[refs.inputMessage.name] = refs.inputMessage.value;
+    userData[refs.inputEmail.type] = refs.inputEmail.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+  }),
+  500
+  
+);
+
+refs.formLogin.addEventListener('submit', onFormHandleSubmit);
+
+function onFormHandleSubmit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+}
